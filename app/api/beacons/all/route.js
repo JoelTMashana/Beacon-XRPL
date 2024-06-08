127ddbcc008366f06f2f31ba30730af6ea41c92a
JoelTMashana <joeltmashana@gmail.com>
2024-06-08 20:16:20 +0200

    create the get all beacons get req

---
+++ app/api/beacons/all/route.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import sqlite3 from 'sqlite3';
+
+export async function GET() {
+    return new Promise((resolve, reject) => {
+        const db = new sqlite3.Database('./mydb.sqlite', sqlite3.OPEN_READWRITE, (err) => {
+            if (err) {
+                reject(NextResponse.json({ error: 'Failed to connect to the database' }, { status: 500 }));
+            } else {
+                db.all("SELECT beacon_id, user_id, content, timestamp FROM Beacons", [], (err, rows) => {
+                    if (err) {
+                        reject(NextResponse.json({ error: err.message }, { status: 500 }));
+                    } else {
+                        resolve(NextResponse.json({ beacons: rows }, { status: 200 }));
+                    }
+                    db.close();
+                });
+            }
+        });
+    });
+}
